ba86b525447155e913afde958782ed6dad40a8f4
kangminsung <nonasking@gmail.com>
2025-04-21 12:17:40 +0900

    전체 코드 리팩토링

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	"log"
 	"os"
 
+	"github.com/gin-gonic/gin"
+	"github.com/go-jcklk/crow/internal/config"
 	"github.com/go-jcklk/crow/internal/handler"
 	"github.com/go-jcklk/crow/internal/notion"
 )
 
 func main() {
-	_ = godotenv.Load()
+	config.LoadConfig()
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -19,13 +19,12 @@ func main() {
 	}
 
 	r := gin.Default()
-	notionClient := notion.NewClient()
+	notionClient := notion.NewClient(config.AppConfig)
 
-	r.POST("/webhook", handler.WebhookHandler(notionClient))
+	r.POST("/webhook", handler.NewWebhookHandler(notionClient))
 
-	log.Println("서버 시작: http://localhost:" + port)
-	err := r.Run(":" + port)
-	if err != nil {
+	log.Printf("서버 시작: http://localhost:%s", port)
+	if err := r.Run(":" + port); err != nil {
 		log.Fatal(err)
 	}
 }
